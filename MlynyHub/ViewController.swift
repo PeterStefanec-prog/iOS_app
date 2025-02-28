5c9aa14a0e80bc2f35701aa1383a80ffacbc095c
Peter1001 <xstefanec@stuba.sk>
2025-02-28 20:11:43 +0100

    Initial Commit

---
+++ MlynyHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MlynyHub
+//
+//  Created by Peter Štefanec on 28/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MlynyHubTests/MlynyHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  MlynyHubTests.swift
+//  MlynyHubTests
+//
+//  Created by Peter Štefanec on 28/02/2025.
+//
+
+import Testing
+@testable import MlynyHub
+
+struct MlynyHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
