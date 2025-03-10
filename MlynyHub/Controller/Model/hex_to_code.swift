b53ba34904defd6f23f3f026ffd6a385a30be951
xKazimir <andrejkazimir.sk@gmail.com>
2025-03-10 11:02:25 +0100

    testing...

---
+++ MlynyHub/Controller/Model/hex_to_code.swift
@@ -0,0 +1,25 @@
+//
+//  hex_to_code.swift
+//  MlynyHub
+//
+//  Created by Andrej on 09/03/2025.
+//
+import UIKit
+
+extension UIColor {
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        let r = CGFloat((rgb >> 16) & 0xFF) / 255.0
+        let g = CGFloat((rgb >> 8) & 0xFF) / 255.0
+        let b = CGFloat(rgb & 0xFF) / 255.0
+
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}
+
