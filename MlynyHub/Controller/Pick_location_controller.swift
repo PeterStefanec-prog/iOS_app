1c0d8da73dfb79354f86df0f989f133ec49bde5b
Kazimir_ <andrejkazimir.sk@gmail.com>
2025-03-20 14:50:59 +0100

    added map, choosing location via pin/address, storing location with the event attributes

---
+++ MlynyHub/Controller/Pick_location_controller.swift
@@ -0,0 +1,29 @@
+//
+//  Pick_location_controller.swift
+//  MlynyHub
+//
+//  Created by Andrej Kazimir on 20/03/2025.
+//
+
+import UIKit
+
+class Pick_location_controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
